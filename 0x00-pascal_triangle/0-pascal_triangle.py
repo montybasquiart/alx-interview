44634c18d88d69fe4cca55679d2c77d251bc5aa4
montybasquiart <owamathias@gmail.com>
2024-10-04 01:37:49 +0000

    Fixed code outpur error

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,17 +1,29 @@
-#!/usr/bin/python3  
-"""Triangle Triangle"""  
+#!/usr/bin/python3
+"""This moodule defines the function pascal_triangle which
+prints a pascal triangle
+"""
 
-def pascal_triangle(n):  
-    """Triangle"""  
-    if n <= 0:  
-        return []  
-    pas_tri = [[1]]  
-    for row_number in range(1, n):  
-        row = [1]  
-        for j in range(1, row_number):  
-            element = pas_tri[row_number - 1][j - 1] + pas_tri[row_number - 1][j]  
-            row.append(element)  
-        row.append(1)  
-        pas_tri.append(row)  
 
-    return pas_tri
+def pascal_triangle(n):
+    """ This function returns a list of integers representing the
+        Pascal's triangle of n.
+    """
+    pascal_triangle = []
+    for i in range(n):
+        row = []
+        for j in range(i + 1):
+            if i == 0:
+                row.append(1)
+                continue
+            else:
+                leftupper = (
+                    0 if i-1 < 0 or j-1 < 0 else
+                    pascal_triangle[i - 1][j - 1]
+                )
+                rightupper = (
+                    0 if i-1 < 0 or j >= len(pascal_triangle[i - 1]) else
+                    pascal_triangle[i - 1][j]
+                    )
+                row.append(leftupper + rightupper)
+        pascal_triangle.append(row)
+    return (pascal_triangle)
