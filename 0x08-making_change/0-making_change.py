3c5f096fe388b1b11c0a6a02ee528bce8faa702c
Mathias Owa Martins <owamathias@gmail.com>
2025-01-17 23:20:37 +0100

    Change comes from within

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,21 +1,43 @@
 #!/usr/bin/python3
 """
-Function to determine the minimum number of coins needed to meet a given amount
+Making Change
 """
 
 
-def makeChange(coins, amount):
+def makeChange(coins, total):
     """
-    Determine the minimum number of coins needed to meet a given amount.
+    Return the minimum number of coins needed to meet a given total
+    Args:
+        coins (list of ints): a list of coins of different values
+        total (int): total value to be met
+    Return:
+        Number of coins or -1 if meeting the total is not possible
     """
-    if amount < 1:
+    if total <= 0:
         return 0
+    if coins == [] or coins is None:
+        return -1
+    try:
+        n = coins.index(total)
+        return 1
+    except ValueError:
+        pass
+
     coins.sort(reverse=True)
-    count = 0
-    for coin in coins:
-        if amount == 0:
-            break
-        num = amount // coin
-        amount -= num * coin
-        count += num
-    return count if amount == 0 else -1
+    coin_count = 0
+    for i in coins:
+        if total % i == 0:
+            coin_count += int(total / i)
+            return coin_count
+        if total - i >= 0:
+            if int(total / i) > 1:
+                coin_count += int(total / i)
+                total = total % i
+            else:
+                coin_count += 1
+                total -= i
+                if total == 0:
+                    break
+    if total > 0:
+        return -1
+    return coin_count
